a34b89a1c1ab89319e19d943f718d84e85da345e
Nicolas Paris <nicolas.paris@healint.com>
2020-06-02 17:59:21 +0800

    cli for versions

--- slack_secret/main.py
+++ slack_secret/main.py
@@ -20,20 +20,29 @@ os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 @click.group()
 @click.version_option(version=__version__)
 @click.argument("token", type=str)
-def cli(token):
-    # slack_token = os.environ["SLACK_API_TOKEN"]
+def main(token):
     global client
     client = WebClient(token=token)
 
 
-@cli.command(help="save all users as json and download the images")
+@click.group()
+def no_token():
+    pass
+
+
+@no_token.command(help="Shows version")
+def version():
+    print(__version__)
+
+
+@main.command(help="save all users as json and download the images")
 def save_users():
     from slack_secret.users import save_users
 
     save_users(client)
 
 
-@cli.command(
+@main.command(
     help="Lists all the private channels that your token can access. You'll need the channel ID for any othe action"
 )
 def list_private_channels():
@@ -42,7 +51,7 @@ def list_private_channels():
     list_private_channels(client)
 
 
-@cli.command(help="Downloads all the messages and images from the supplied channelID")
+@main.command(help="Downloads all the messages and images from the supplied channelID")
 @click.argument("channel_id", type=str)
 def save_private_channel(channel_id):
     from slack_secret.channels import save_private_channel
@@ -50,20 +59,22 @@ def save_private_channel(channel_id):
     save_private_channel(client, channel_id=channel_id)
 
 
-@cli.command(help="Downloads all the messages and images from all channels")
+@main.command(help="Downloads all the messages and images from all channels")
 def save_all_private_channels():
     from slack_secret.channels import save_all_private_channels
 
     save_all_private_channels(client)
 
 
-@cli.command(help="Deletes all the messages and from the supplied channelID")
+@main.command(help="Deletes all the messages and from the supplied channelID")
 @click.argument("channel_id", type=str)
 def delete_private_channel(channel_id):
     from slack_secret.channels import delete_private_channel
 
     delete_private_channel(client, channel_id=channel_id)
 
+
+cli = click.CommandCollection(sources=[main, no_token])
 
 if __name__ == "__main__":
     cli()
